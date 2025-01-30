2b8381b161fb81e58c6477717871b4d9c650748e
Iago Nunes <iagonunesfrancino@hotmail.com>
2025-01-30 03:34:17 -0300

    Teste adicionar nome na lista

--- app.js
+++ app.js
@@ -1,1 +1,14 @@
-// testendo push do jogo do amigo secreto
+let nomeAmigos = [];
+
+function adicionarAmigo(){
+    let nome = document.getElementById("amigo").value;
+    if(nome.trim() === ""){
+        alert('digite um nome válido');
+    }else{
+        document.getElementById("listaAmigos").innerHTML = nome;
+    }
+    
+}
+// até aqui já temos um trecho do código que consegue apresentar o nome digitado na listaAmigos, preciso ver como armazenar os nomes
+// no array e após armazenar, limpar o campo de texto para receber um novo nome.
+
